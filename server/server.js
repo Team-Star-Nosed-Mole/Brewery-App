f5f227a18d193fa125555ab3102bc6e7841cb22f
Colton Robbins <coltondr@gmail.com>
2022-04-23 16:14:58 -0500

    mm

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+const userController = require("./userController")
+
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
@@ -10,6 +12,15 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.status(200).sendFile(path.join(__dirname, "../client/template.html"));
 });
+
+app.post('/newUser', userController.createUser,  (req, res) => {
+  res.json(res.locals.users);
+})
+
+app.post('/login', userController.verifyLogin, userController.setCookie, (req, res) => {
+  console.log('login success!')
+  res.send('login success');
+})
 
 module.exports = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
