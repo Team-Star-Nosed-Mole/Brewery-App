56e4c705c3d9fcc6e5a716bf89b176b04ee73b2a
Peter Steinberg <bklynpeter@gmail.com>
2022-04-22 20:12:57 -0400

    work on bundle

--- server/server.js
+++ server/server.js
@@ -7,6 +7,10 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, "../client/template.html"));
+});
+
 module.exports = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
